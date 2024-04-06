c0f86d2ab2ee8c4f33140db8c09108f9d07a17ab
ZabR19 <zabdi.ramirez@lionintel.com>
2024-04-06 10:21:59 -0600

    first commit

---
+++ class/connection_class.php
@@ -0,0 +1,36 @@
+<?php
+
+class DBConnection {
+
+    private $host;
+    private $user;
+    private $password;
+    private $db;
+    private $conn;
+
+    public function __construct()
+    {
+        $this->host = "localhost";
+        $this->user = "root";
+        $this->password = "";
+        $this->db = "";
+
+        // Crear conexión
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db);
+
+        // Verificar la conexión
+        if ($this->conn->connect_error) {
+            die("Conexión fallida: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        $this->conn->close();
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/general.css"/>
+    <link rel="stylesheet" href="css/sweetalert2.min.css"/>
+</head>
+<body>
+    <h1>Welcome</h1>
+   
+    <section>
+        
+    </section>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
+    <script src="js/ajax.js"></script>
+</body>
+</html>
